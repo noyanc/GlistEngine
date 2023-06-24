a85549721aef31d6bb24b822c77a925e5ef1cb85
noyanc <noyanculum@yahoo.com>
2023-06-24 00:12:13 +0000

    Deploying to gh-pages from @ noyanc/GlistEngine@daca9b54b683508d4d7aa90ed4c23d3427f72d24 🚀

---
+++ navtreedata.js
@@ -0,0 +1,59 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "GlistEngine", "index.html", [
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classgBaseParticles.html#a5a03a5f98b25c206b131a815ee174c9b",
+"classgDatabase.html#ac5c3ef991f96d9f8de3a1d840d9ead5a",
+"classgGUIContainer.html#a123213757845c1357859305d988b0d1f",
+"classgGUIImageButton.html#a12e7b7a2618eee3bffeb598e2ecb1dce",
+"classgGUIResources.html",
+"classgImage.html#a7a81937ff1062436da3565726234936f",
+"classgModel.html#a9bf31d28f047db9161e17461ca0cd702",
+"classgRenderer.html#ab2efab9124d8796f6792e2e3349df29f",
+"classgThread.html#ad647ecf3b4c701098d585e724edb38b8"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
